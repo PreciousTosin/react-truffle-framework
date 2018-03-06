29b19e89192bf393b60cd01f9621afdacdee845f
precioustosin <precioustosin@hotmail.com>
2018-03-06 11:53:38 +0100

    corrected lint errors

--- src/store.js
+++ src/store.js
@@ -1,22 +1,17 @@
-import { browserHistory } from 'react-router'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { routerMiddleware } from 'react-router-redux'
-import reducer from './reducer'
+/* eslint no-underscore-dangle: 'off' */
+import { browserHistory } from 'react-router';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
+import reducer from './reducer';
 
 // Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const routingMiddleware = routerMiddleware(browserHistory)
+const routingMiddleware = routerMiddleware(browserHistory);
 
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(
-      thunkMiddleware,
-      routingMiddleware
-    )
-  )
-)
+const store = createStore(reducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware,
+    routingMiddleware)));
 
-export default store
+export default store;
