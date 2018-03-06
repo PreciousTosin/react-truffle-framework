a4f704eb682da086fa6dff291a95379d1da52a17
precioustosin <precioustosin@hotmail.com>
2018-03-06 13:26:25 +0100

    "fixed lint errors, changed file extensions

---
+++ src/layouts/dashboard/Dashboard.jsx
@@ -0,0 +1,29 @@
+/* eslint react/prop-types: 'off', react/no-unescaped-entities: 'off',
+no-param-reassign: 'off', no-unused-vars: 'off' */
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor(props, { authData }) {
+    super(props);
+    authData = this.props;
+  }
+
+  render() {
+    return (
+      <main className="container">
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <h1>Dashboard</h1>
+            <p>
+              <strong>Congratulations {this.props.authData.name}!</strong>
+              If you're seeing this page, you've logged in with your own smart
+              contract successfully.
+            </p>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Dashboard;
