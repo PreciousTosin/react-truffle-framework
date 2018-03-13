6766249c68d0e37cbb6f8ecb7876cb3627f11b57
precioustosin <precioustosin@hotmail.com>
2018-03-13 16:46:13 +0100

    restructured components, all tests passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter as Router } from 'react-router-redux';
 import App from './App';
+import store, { history } from './store';
 
 it('renders without crashing', () => { /* eslint no-undef: 'off' */
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>), div);
 });
