fd6e392409fec30797d332f40c28d23bf14c59a3
precioustosin <precioustosin@hotmail.com>
2018-03-13 17:31:59 +0100

    fixed lint errors

--- test/authentication.js
+++ test/authentication.js
@@ -1,17 +1,12 @@
-var Authentication = artifacts.require("./Authentication.sol");
+/* eslint no-undef: 'off'  */
+const Authentication = artifacts.require('./Authentication.sol');
 
-contract('Authentication', function(accounts) {
+contract('Authentication', (accounts) => {
+  it('...should sign up and log in a user.', () => Authentication.deployed().then((instance) => {
+    authenticationInstance = instance;
 
-  it("...should sign up and log in a user.", function() {
-    return Authentication.deployed().then(function(instance) {
-      authenticationInstance = instance;
-
-      return authenticationInstance.signup('testuser', {from: accounts[0]});
-    }).then(function() {
-      return authenticationInstance.login.call();
-    }).then(function(userName) {
-      assert.equal(web3.toUtf8(userName), 'testuser', "The user was not signed up.");
-    });
-  });
-
+    return authenticationInstance.signup('testuser', { from: accounts[0] });
+  }).then(() => authenticationInstance.login.call()).then((userName) => {
+    assert.equal(web3.toUtf8(userName), 'testuser', 'The user was not signed up.');
+  }));
 });
